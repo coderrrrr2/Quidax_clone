08411e5d203ac6ba1ed4c19af92b402d3db98ba0
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2025-01-29 18:34:42 +0100

    Initial Commit

---
+++ Quidax_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quidax_clone
+//
+//  Created by Adebayo Philip on 29/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quidax_cloneTests/Quidax_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Quidax_cloneTests.swift
+//  Quidax_cloneTests
+//
+//  Created by Adebayo Philip on 29/01/2025.
+//
+
+import Testing
+@testable import Quidax_clone
+
+struct Quidax_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
